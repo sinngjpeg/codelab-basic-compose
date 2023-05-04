8688c1505ff34a035b1f7dc020ef7f24eac47e3e
ingrid silva <ingridsilvatec@outlook.com>
2023-05-04 03:22:04 -0300

    add parameters nedds

--- BasicCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ BasicCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BasicoComposeApp()
                 }
             }
         }
@@ -29,17 +30,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicoComposeApp() {
+    
+}
+
+
+@Composable
+fun BasicComposeArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BasicComposeArticlePreview() {
     BasicComposeTheme {
-        Greeting("Android")
+        BasicoComposeApp()
     }
 }
